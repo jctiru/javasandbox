7b199158893620b9660eb6dc6a35ec24a008e14f
jctiru <jonathanchristiantiru@gmail.com>
2019-07-23 16:36:22 +0800

    Add parallel examples in ArraysDemo.java

--- javasandbox/src/test/javasandbox/collections/ArraysDemo.java
+++ javasandbox/src/test/javasandbox/collections/ArraysDemo.java
@@ -3,6 +3,8 @@ package test.javasandbox.collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
 
 public class ArraysDemo {
 
@@ -80,8 +82,51 @@ public class ArraysDemo {
 		System.out.println(ia[0][2]);
 	}
 
+	// Java 8 ~ Parallelized Operations
+	private static void parallel() {
+		// For large arrays on multi-core. Min size atleast 1 >> 13 = 8192
+		int[] iArray = { 23, 4, 59 };
+		Arrays.parallelSort(iArray);
+		System.out.println("iArray parallel sort: " + Arrays.toString(iArray));
+
+		IntBinaryOperatorImpl IntBinaryOperatorImpl = new IntBinaryOperatorImpl();
+		Arrays.parallelPrefix(iArray, IntBinaryOperatorImpl);
+		System.out.println("Parallel Prefix: " + Arrays.toString(iArray));
+
+		IntUnaryOperatorImpl intUnaryOperatorImpl = new IntUnaryOperatorImpl();
+		intUnaryOperatorImpl.setArray(iArray);
+		Arrays.parallelSetAll(iArray, intUnaryOperatorImpl);
+		System.out.println("Parallel Set All: " + Arrays.toString(iArray));
+	}
+
 	public static void main(String[] args) {
-		sequential();
+//		sequential();
+		parallel();
+	}
+
+}
+
+class IntBinaryOperatorImpl implements IntBinaryOperator {
+	@Override
+	public int applyAsInt(int left, int right) {
+		return left + right;
+	}
+}
+
+class IntUnaryOperatorImpl implements IntUnaryOperator {
+	int[] iArray;
+
+	public void setArray(int[] iArray) {
+		this.iArray = iArray;
+	}
+
+	@Override
+	public int applyAsInt(int i) {
+		if (iArray != null) {
+			return iArray[i] + 5;
+		} else {
+			return i;
+		}
 	}
 
 }
