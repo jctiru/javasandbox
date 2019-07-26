d98741894d701286dafd0b0add4d666a26c5e3c3
jctiru <jonathanchristiantiru@gmail.com>
2019-07-26 19:29:20 +0800

    Add state behavior demo to enum

--- javasandbox/src/test/javasandbox/enums/BookGenre.java
+++ javasandbox/src/test/javasandbox/enums/BookGenre.java
@@ -1,7 +1,17 @@
 package test.javasandbox.enums;
 
 public enum BookGenre {
-	BIOGRAPHY, HORROR;
+	BIOGRAPHY(12), HORROR(15);
+
+	private BookGenre(int minAgeToRead) {
+		this.minAgeToRead = minAgeToRead;
+	}
+
+	private int minAgeToRead;
+
+	public int getMinAgeToRead() {
+		return minAgeToRead;
+	}
 
 	public static void main(String[] args) {
 		for (BookGenre bookGenre : BookGenre.values()) {
@@ -11,6 +21,7 @@ public enum BookGenre {
 			System.out.print(", Declaring Class: " + bookGenre.getDeclaringClass());
 			System.out.print(", compareTo(HORROR): " + bookGenre.compareTo(BookGenre.HORROR));
 			System.out.print(", equals(HORROR): " + bookGenre.equals(BookGenre.HORROR));
+			System.out.print(", minAgeToRead: " + bookGenre.getMinAgeToRead());
 		}
 	}
 }
