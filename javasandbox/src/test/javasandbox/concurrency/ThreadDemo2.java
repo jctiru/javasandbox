69eb6be500e2c872c18f4f172a15133f771b6f34
jctiru <jonathanchristiantiru@gmail.com>
2019-07-27 11:57:52 +0800

    Add thread demos

---
+++ javasandbox/src/test/javasandbox/concurrency/ThreadDemo2.java
@@ -0,0 +1,25 @@
+package test.javasandbox.concurrency;
+
+public class ThreadDemo2 extends Thread {
+	public void run() {
+		System.out.println("Inside run ...");
+		go();
+	}
+
+	private void go() {
+		System.out.println("Inside go ...");
+		more();
+	}
+
+	private void more() {
+		System.out.println("Inside more ...");
+	}
+
+	public static void main(String[] args) {
+		Thread myThread = new ThreadDemo2();
+		myThread.start();
+
+		System.out.println("Inside main ...");
+	}
+
+}
