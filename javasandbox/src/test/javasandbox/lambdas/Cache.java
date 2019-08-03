02de9a34d1909bba8536db4ee85c2ce57f05d2cf
jctiru <jonathanchristiantiru@gmail.com>
2019-08-03 21:36:53 +0800

    Add capturing variables from lambdas example

--- javasandbox/src/test/javasandbox/lambdas/Cache.java
+++ javasandbox/src/test/javasandbox/lambdas/Cache.java
@@ -1,7 +1,9 @@
 package test.javasandbox.lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 interface CacheIterator {
 	boolean hasNext();
@@ -117,7 +119,44 @@ public class Cache {
 
 	}
 
+	// Mutating it is not thread-safe
+	static int global = 0;
+
 	public void go(Test test) {
 		test.apply();
+
+		// 1. count is effectively final (Capturing variables)
+		int count = 0;
+
+		// 3. Trick & Mutating object is fine.
+		List<Integer> trick = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			// new Thread(() -> System.out.println(count++)).start();
+
+			// 2. Accessing field ~ Not thread safe
+			new Thread(() -> System.out.println(global++)).start();
+
+			new Thread(() -> {
+				trick.add(count);
+				int temp = trick.get(0);
+				trick.set(0, temp++);
+			}).start();
+
+			// 4. No local variable shadowing
+			// Consumer<Integer> consumer = count -> {};
+			// Consumer<Integer> consumer = c -> { int count = 2;};
+
+			// 5. Refresher for Anonymous Classes:
+			new Thread(new Runnable() {
+				public void run() {
+					// count++; // same as lambda
+					int count = 2; // Can shadow local variable
+				}
+			}).start();
+
+			// 6. this reference in lambda is this reference of the enclosed method
+			// new Thread(() -> System.out.println(this.global)).start();
+		}
 	}
 }
