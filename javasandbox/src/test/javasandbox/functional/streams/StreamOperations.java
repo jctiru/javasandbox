3b94fd9c68f049a4787f0e83b8186f9e87a7873f
jctiru <jonathanchristiantiru@gmail.com>
2019-08-05 15:13:49 +0800

    Add stream match examples

--- javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
+++ javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
@@ -100,13 +100,38 @@ public class StreamOperations {
 		// titleStream.forEach(System.out::println);
 	}
 
+	// Queries:
+	// (a) Is there at least one highly rated book (>= 4.8) that is inexpensive (<=
+	// $50)?
+	// (b) Do all the books have a rating >= 4.8
+	// (c) Check if none of books have bad rating (2.0)?
+	private static void match(List<Book> books) {
+		System.out.println("\nMatching ... ");
+		boolean anyMatch = books.stream()
+				.anyMatch(d -> d.getRating() >= 4.8 && d.getPrice() <= 50.0);
+		System.out.println("anyMatch? " + anyMatch);
+
+		boolean allMatch = books.stream()
+				.allMatch(d -> d.getRating() >= 4.8);
+		System.out.println("allMatch? " + allMatch);
+
+		boolean noneMatch = books.stream()
+				.noneMatch(d -> d.getRating() <= 2.0);
+		System.out.println("noneMatch? " + noneMatch);
+
+	}
+
 	public static void main(String[] args) {
 		List<Book> books = new ArrayList<>();
 
 		books.addAll(DataExtractor.getFromAmazon("java"));
 		books.addAll(DataExtractor.getFromBarnesAndNoble("java"));
 
-		slice(books);
+		// Intermediate operations (return stream objects)
+		// slice(books);
+
+		// Terminal operations (return non-stream objects)
+		match(books); // matching stream elements to some criteria
 	}
 
 }
