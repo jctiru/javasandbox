1a362bcaf29f7b14f222d9f32123bee3293adf57
jctiru <jonathanchristiantiru@gmail.com>
2019-08-05 16:05:41 +0800

    Add stream find examples

--- javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
+++ javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
@@ -2,6 +2,7 @@ package test.javasandbox.functional.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -118,7 +119,42 @@ public class StreamOperations {
 		boolean noneMatch = books.stream()
 				.noneMatch(d -> d.getRating() <= 2.0);
 		System.out.println("noneMatch? " + noneMatch);
+	}
 
+	// findFirst needs more work in parallel env. Use findAny if it does the job.
+	// java.util.Optional ~
+	// (a) to avoid dealing with null -- in case of find,
+	// (b) to know if stream is empty -- in case of reduction operation
+	private static void find(List<Book> books) {
+		System.out.println("\nFinding ...");
+
+		Optional<Book> result = books.stream()
+				.filter(d -> d.getRating() >= 4.8 && d.getPrice() <= 50.0)
+				.findAny();
+
+		if (result.isPresent()) {
+			System.out.println(result.get());
+		} else {
+			System.out.println("not found!!!");
+		}
+
+		books.stream()
+				.filter(d -> d.getRating() >= 4.8 && d.getPrice() <= 50.0)
+				.findAny()
+				.orElseGet(StreamOperations::getDefault);
+		// ifPresent(StreamOperations::print);
+
+		Optional<Book> opt = Optional.ofNullable(books.get(0));
+		System.out.println(opt.isPresent());
+	}
+
+	private static void print(Book b) {
+		System.out.println(b);
+	}
+
+	private static Book getDefault() {
+		System.out.println("default ...");
+		return new Book(0, "", 4.0, 25.0, "Amazon");
 	}
 
 	public static void main(String[] args) {
@@ -131,7 +167,10 @@ public class StreamOperations {
 		// slice(books);
 
 		// Terminal operations (return non-stream objects)
-		match(books); // matching stream elements to some criteria
+		// match(books); // matching stream elements to some criteria
+
+		// All matching & finding operations + limit are short-circuit operations
+		find(books);
 	}
 
 }
