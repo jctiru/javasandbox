f6c236c6bcd714edb53f3b895123f5201938b207
jctiru <jonathanchristiantiru@gmail.com>
2019-08-05 22:12:25 +0800

    Add stream reduce examples

--- javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
+++ javasandbox/src/test/javasandbox/functional/streams/StreamOperations.java
@@ -1,6 +1,7 @@
 package test.javasandbox.functional.streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -157,6 +158,54 @@ public class StreamOperations {
 		return new Book(0, "", 4.0, 25.0, "Amazon");
 	}
 
+	// Find the lowest priced book with a rating >= 4.5
+	private static void reduce(List<Book> books) {
+		System.out.println("\nReduce ...");
+		books.stream()
+				.filter(b -> b.getRating() >= 4.5)
+				.reduce((b1, b2) -> b1.getPrice() <= b2.getPrice() ? b1 : b2)
+				.ifPresent(b -> System.out.println("Lowest priced book: " + b));
+	}
+
+	// Limitations:
+	// 1. Cumbersome
+	// 2. Parallelizing is painful
+	// 3. Synchronizing shared mutable variable is expensive
+	private static void reduceImperatively(List<Book> books) {
+		System.out.println("\nReducing imperatively ...");
+		Book result = null;
+
+		for (Book book : books) {
+			// Initialize result with first book having rating >= 4.5
+			if (result == null) {
+				if (book.getRating() >= 4.5) {
+					result = book;
+				}
+				continue;
+			}
+
+			if (book.getRating() >= 4.5 && result.getPrice() > book.getPrice()) {
+				result = book;
+			}
+		}
+
+		System.out.println("(Imperative) Lowest priced book with rating >= 4.5: " + result);
+	}
+
+	static void overloadedReductions() {
+		System.out.println("\noverloadedReductions ... ");
+		String[] grades = { "A", "A", "B" };
+
+		String concat1 = Arrays.stream(grades)
+				.reduce("", (s1, s2) -> s1 + s2);
+		System.out.println("concat1: " + concat1);
+
+		StringBuilder concat2 = Arrays.stream(grades)
+				.reduce(new StringBuilder(), (sb, s) -> sb.append(s),
+						(sb1, sb2) -> sb1.append(sb2));
+		System.out.println("concat2: " + concat2);
+	}
+
 	public static void main(String[] args) {
 		List<Book> books = new ArrayList<>();
 
@@ -170,7 +219,11 @@ public class StreamOperations {
 		// match(books); // matching stream elements to some criteria
 
 		// All matching & finding operations + limit are short-circuit operations
-		find(books);
+		// find(books);
+
+		reduce(books);
+		reduceImperatively(books);
+		overloadedReductions();
 	}
 
 }
