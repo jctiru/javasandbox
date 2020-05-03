545dc722054ee799e9bc7f328c1f10e947197f6a
jctiru <jonathanchristiantiru@gmail.com>
2020-05-03 18:58:52 +0800

    Add another method reference example

---
+++ javasandbox/src/test/javasandbox/functional/unit3/MethodReferenceExample2.java
@@ -0,0 +1,35 @@
+package test.javasandbox.functional.unit3;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+import test.javasandbox.functional.unit1.Person;
+
+public class MethodReferenceExample2 {
+
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(
+				new Person("Charles", "Dickens", 60),
+				new Person("Lewis", "Carroll", 42),
+				new Person("Thomas", "Carlyle", 51),
+				new Person("Charlotte", "Bronte", 45),
+				new Person("Matthew", "Arnold", 39));
+
+		System.out.println("Printing all people");
+		performConditionally(people, p -> true, System.out::println);
+
+	}
+
+	private static void performConditionally(List<Person> people, Predicate<Person> predicate,
+			Consumer<Person> consumer) {
+		for (Person p : people) {
+			if (predicate.test(p)) {
+				consumer.accept(p);
+			}
+		}
+	}
+
+}
